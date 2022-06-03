5cc414e372302372065a77f2451bc579ee371cd8
narutocodinja <97546063+narutocodinja@users.noreply.github.com>
2022-06-03 22:33:34 +0530

    Create twoSum.cpp

---
+++ Arrays/twoSum.cpp
@@ -0,0 +1,16 @@
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map <int, int> ref;
+        vector<int> result;
+        
+        for(int i = 0; i < nums.size(); i++){
+            int req_num = target - nums[i];
+            if(ref.find(req_num) != ref.end()){
+                result.push_back(ref[req_num]);
+                result.push_back(i);
+                return result;
+            }
+            ref[nums[i]] = i;
+        }
+        return result;
+    }
