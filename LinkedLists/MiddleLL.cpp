4859db00071b9794966d35f740396d259c5f8876
narutocodinja <97546063+narutocodinja@users.noreply.github.com>
2022-06-05 22:14:25 +0530

    Create MiddleLL.cpp

---
+++ LinkedLists/MiddleLL.cpp
@@ -0,0 +1,11 @@
+ListNode* middleNode(ListNode* head) {
+        if(head == nullptr) return head;
+        
+        ListNode *slow = head, *fast = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
